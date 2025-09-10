01e722aeabca82b363333cd5d226b2220ee195d0
chenc319 <87655286+chenc319@users.noreply.github.com>
2025-09-10 12:09:06 -0500

    Update app_fed_balance_sheet.py

--- app_fed_balance_sheet.py
+++ app_fed_balance_sheet.py
@@ -139,7 +139,7 @@ def plot_fed_balance_sheet_liabilities(start, end, **kwargs):
     st.plotly_chart(fig, use_container_width=True)
 
     ### PLOT ###
-    fig = make_subplots(rows=2, cols=3, subplot_titles=labels)
+    fig = make_subplots(rows=3, cols=2, subplot_titles=labels)
     for i, (col, color, label) in enumerate(zip(cols, colors, labels)):
         row = i // 3 + 1
         col_position = i % 3 + 1
