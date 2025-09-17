0b63cde29e504ba41e6873f967bfac929b5f41b3
chenc319 <87655286+chenc319@users.noreply.github.com>
2025-09-17 15:00:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -277,7 +277,7 @@ elif menu == 'TRACE Model':
     st.title('On-the-Run vs. Off-the-Run')
     app_trace.plot_on_vs_off(start_date,end_date)
     st.title('Dealer-to-Dealer vs Dealer-to-Client')
-    app_trace.plot_on_vs_off(start_date, end_date)
+    app_trace.plot_dealer_dealer_vs_dealer_client(start_date, end_date)
     st.title('On-the-Run Nominal Coupons')
     app_trace.plot_on_the_run_nominal_coupons(start_date,end_date)
     st.title('Off-the-Run Nominal Coupons')
