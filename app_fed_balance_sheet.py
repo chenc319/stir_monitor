20c8b8792eb10140cc0bb2a2a420b51738cbd6f8
chenc319 <87655286+chenc319@users.noreply.github.com>
2026-03-12 01:42:20 -0500

    Update app_fed_balance_sheet.py

--- app_fed_balance_sheet.py
+++ app_fed_balance_sheet.py
@@ -93,6 +93,11 @@ def plot_fed_balance_sheet_snapshot(start, end, **kwargs):
     base_series = fed_balance_sheet_dict["Reserve Bank Credit"]
     all_dates = base_series.index.sort_values()
 
+    ### ROUND ALL NUMBERS ###
+    for key, obj in fed_balance_sheet_dict.items():
+        if isinstance(obj, (pd.DataFrame, pd.Series)):
+            fed_balance_sheet_dict[key] = obj.round(0)
+
     chosen_date = st.selectbox(
         "Select H.4.1 date",
         options=all_dates,
